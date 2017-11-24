8e85dda87dd92b185fe70cb5862a51e255bf054e
AJ Javadi <amirjavadi25@gmail.com>
2017-11-24 05:04:50 -0600

    Add files via upload

---
+++ sample_programs.py
@@ -0,0 +1,6 @@
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+total = number1 + number2 + number3
+average = total/3
+print("The average is " + str(average))
